b13177dbd7a2096fbaecbacae78e40aca139ea8f
Tinh Ngo <tinh.ngo@niteco.se>
2019-02-20 16:48:24 +0700

    add module Niteco_Oracle

--- app/code/Mastering/SampleModule/Block/Hello.php
+++ app/code/Mastering/SampleModule/Block/Hello.php
@@ -3,20 +3,25 @@
 namespace Mastering\SampleModule\Block;
 
 use Magento\Framework\View\Element\Template;
-use Mastering\SampleModule\Model\ResourceModel\Item\Collection;
 use Mastering\SampleModule\Model\ResourceModel\Item\CollectionFactory;
 
 class Hello extends Template {
 
     private $collectionFactory;
+    protected $_orderCollectionFactory;
+    protected $_orderRepository;
 
     public function __construct(
         Template\Context $content,
         CollectionFactory $collectionFactory,
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
         array $data = []
     )
     {
         $this->collectionFactory = $collectionFactory;
+        $this->_orderCollectionFactory = $orderCollectionFactory;
+        $this ->_orderRepository = $orderRepository;
         parent::__construct($content, $data);
     }
 
@@ -25,6 +30,16 @@ class Hello extends Template {
      */
     public function getItems() {
         return $this->collectionFactory->create()->getItems();
+    }
+
+    public function getAllOrders() {
+        $result = $this->_orderCollectionFactory->create()->addAttributeToSelect('*');
+        $data = $result->addFieldToFilter('status', 'pending');
+        return $data;
+    }
+
+    public function getOrderById() {
+       return 'end';
     }
 
 }
---
+++ app/code/Niteco/Oracle/Setup/InstallSchema.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Niteco Co Ltd.
+ * User: tinh.ngo
+ * Date: 2/20/2019
+ * Time: 3:43 PM
+ */
+
+namespace Niteco\Oracle\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface {
+
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $setup->getConnection()->addColumn(
+            $setup->getTable('sales_order'),
+            'sent_to_oracle',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                'length' => 10,
+                'nullable' => true,
+                'default' => '0',
+                'comment' => 'Custom attribute for Oracle'
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
