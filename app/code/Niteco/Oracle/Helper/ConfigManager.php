ca495aee42c1d6fef63301ca7099e730e41c87d5
Tinh Ngo <it.tinhngo@gmail.com>
2019-03-04 23:37:34 +0700

    done send mail

--- app/code/Niteco/Oracle/Helper/ConfigManager.php
+++ app/code/Niteco/Oracle/Helper/ConfigManager.php
@@ -13,18 +13,15 @@ use Magento\Framework\App\Helper\AbstractHelper;
 class ConfigManager extends AbstractHelper {
 
     protected $scopeConfig;
-    private $email;
     private $sentEmailLogger;
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Niteco\Oracle\Common\Email $email,
         \Niteco\Oracle\Common\SentEmailLogger $sentEmailLogger
     )
     {
         $this->scopeConfig = $scopeConfig;
-        $this->email = $email;
         $this->sentEmailLogger = $sentEmailLogger;
 
         parent::__construct($context);
@@ -43,69 +40,6 @@ class ConfigManager extends AbstractHelper {
         return $this->getConfig('send_mail', 'enabled');
     }
 
-    public function sendMail($message) {
-
-        /**
-         * Fetch the e-mail address(es)
-         */
-        $emails = explode(';', $this->getEmailReceive());
-
-
-        /**
-         * Remove any whitespace
-         */
-        array_walk($emails, 'trim');
-
-
-        /**
-         * Fetch the general e-mail address
-         */
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
-        $generalEmail   = $this->scopeConfig->getValue('trans_email/ident_general/email', $storeScope);
-        $generalName  = $this->scopeConfig->getValue('trans_email/ident_general/name', $storeScope);
-
-
-        /**
-         * If nothing was found, use our default
-         */
-        if (!$generalEmail) {
-            $generalEmail = 'tinh.ngo@niteco.se';
-        }
-
-        if (!$generalName) {
-            $generalName = 'Magento';
-        }
-
-        /**
-         * Send message to each recipient
-         */
-        foreach ($emails as $emailAddress) {
-            try {
-
-                $emailTempVariables = array();
-                $emailTempVariables['message'] = $message;
-                $senderInfo = [
-                    'name' => $generalName,
-                    'email' => $generalEmail,
-                ];
-                $receiverInfo = [
-                    'name' => 'Receiver',
-                    'email' => $emailAddress
-                ];
-
-
-                $this->email->yourCustomMailSendMethod(
-                    $emailTempVariables,
-                    $senderInfo,
-                    $receiverInfo
-                );
-
-            } catch (Exception $e) {
-                $this->sentEmailLogger->logText($e->getMessage());
-            }
-        }
-
-    }
 
     /**
      * Fetch configuration
